3e833c40954a71cdedfb42925a452cad9f2b908c
lpaiva <paivafleandro@gmail.com>
2025-08-28 18:25:22 +0100

    Atualização do CORS origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     # "http://127.0.0.1:3000",
-    "https://synchro-gest.vercel.app/"
+    "https://synchro-gest.vercel.app"
 ]
 
 app.add_middleware(
